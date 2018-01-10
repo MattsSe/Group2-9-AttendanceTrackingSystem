3f769766b25dfe00eed1130c37b13220def9bc1e
Matthias <matthias.seitz@tum.de>
2018-01-10 14:46:31 +0100

    fixed api host endpoint

--- ase-python-api/swagger_client/configuration.py
+++ ase-python-api/swagger_client/configuration.py
@@ -45,7 +45,7 @@ class Configuration(object):
         Constructor
         """
         # Default Base url
-        self.host = "https://ase2017-group-2-9.appspot.com"
+        self.host = "https://ase2017-group-2-9.appspot.com/api"
         # Default api client
         self.api_client = None
         # Temp file folder for downloading files
